732c8f912797ab57d463df642f6851705f849cce
Hydrozoa <ctjohnston@rogers.com>
2012-05-08 14:18:59 -0300

    PUSS OF SAILOR'S INGROWN NAIL PUTRID SKIN OF WASHED UP WHALE

--- src/Tutor.java
+++ src/Tutor.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 
 
 public class Tutor {
-
-	String name;
-	ArrayList<String> tutorCourses;
-	int grade;
-	Schedule schedule = new Schedule(new boolean[] {}, new boolean[] {});
-	
+	/**
+	 * FIRST OF ALL, IAN IS A BIG FAT MEANIE THAT HATES ARTISTIC EXPRESSION. D:<
+	 * This method allows for the creation of the tutor object that stores all the information about the tutor.
+	 */
+	String name;//the tutors name
+	ArrayList<String> tutorCourses;//the courses the tutor can tutor
+	int grade;//the tutors grade
+	Schedule schedule = new Schedule(new boolean[] {}, new boolean[] {});//the tutors schedule
+	//this is the constructor method that allows the tutor to be created and all their information set
 	Tutor(String n, String[] courses, int gr, Schedule sch)
 	{
 		name = n;
@@ -15,42 +18,42 @@ public class Tutor {
 		grade = gr;
 		schedule = sch;
 	}
-	
+	//returns the tutors name
 	public String getName()
 	{
 		return name;
 	}
-	
+	//returns the courses the tutor can tutor
 	public ArrayList<String> getTutorCourses()
 	{
 		return tutorCourses;
 	}
-	
+	//returns the tutors grade
 	public int getGrade()
 	{
 		return grade;
 	}
-	
+	//returns the tutors schedule
 	public Schedule getSchedule()
 	{
 		return schedule;
 	}
-	
+	//changes the tutors name
 	public void setName(String n)
 	{
 		name = n;
 	}
-	
+	//changes the courses the tutor can tutor
 	public void setTutorCourses(ArrayList<String> courses)
 	{
 		tutorCourses = courses;
 	}
-	
+	//changes the tutors schedule
 	public void setSchedule(Schedule schedule2)
 	{
 		schedule = schedule2;
 	}
-	
+	//returns all of the tutors information
 	public String toString()
 	{
 		return name + ", " + grade;
