c19b5926076ff36436979190a0fc061c69f58418
Peter Kim <peterdiane65@gmail.com>
2012-11-22 00:21:16 -0500

    Filled in the Tutor and Student Classes
    IM DONE HERE, PEACE

--- src/controlClasses/Student.java
+++ src/controlClasses/Student.java
@@ -10,79 +10,87 @@ public class Student {
 	private String studentNumber;
 	private String name;//the students name
 	private String password;
-	private String courses[];//the students courses
+	private ArrayList<String> courses;//the students courses
 	private int grade;//the students grade
 	private ArrayList<Lesson> lessons;
 	
-	public Student()
+	public Student(String sNum, String n, String pass, ArrayList<String> c, int gr, ArrayList<Lesson> l)
 	{
-		
+		studentNumber =sNum;
+		name=n;
+		password=pass;
+		courses=c;
+		grade=gr;
+		lessons=l;
 	}
 
 	public String getStudentNumber()
 	{
-		
+		return(studentNumber);
 	}
 	public String getName()
 	{
-		
+		return(name);
 	}
 	public boolean verifyPassword(String pass)
 	{
-		
+		if(pass.equals(name))
+			return true;
+		return false;
 	}
-	public ArrayList<String> getCourses()
+	public ArrayList<String>  getCourses()
 	{
-		
+		return courses;
 	}
 	public int getGrade()
 	{
-		
+		return grade;
 	}
 	public ArrayList<Lesson> getLessons()
 	{
-		
+		return lessons;
 	}
 
 	public void setStudentNumber(String sNum)
 	{
-		
+		studentNumber = sNum;
 	}
 	public void setName(String n)
 	{
-		
+		name =n;
 	}
 	public void setPassword(String pass)
 	{
-		
+		password = pass;
 	}
-	public void setCourses(ArrayList<String> courses)
+	public void setCourses(ArrayList<String> course)
 	{
-		
+		courses = course;
 	}
 	public void addCourses(String course)
 	{
-		
+		courses.add(course);
 	}
 	public void removeCourses(String course)
 	{
-		
+		courses.remove(course);
 	}
 	public void setGrade(int g)
 	{
-		
+		grade =g;
 	}
 	public void addLesson(Lesson l)
 	{
-		
+		lessons.add(l);
 	}
 	public void removeLesson(Lesson l)
 	{
-		
+		lessons.remove(l);
 	}
 
 	public String toString()
 	{
-		
+		return "Name: " + name +"/n studentNumber: " +studentNumber + 
+				"/n Courses: " + courses.toString() + "/n Lessons: " + lessons.toString();
 	}
 }
--- src/controlClasses/Tutor.java
+++ src/controlClasses/Tutor.java
@@ -17,103 +17,114 @@ public class Tutor {
 	private ArrayList<Request> requests;
 	private boolean occupied;
 	
-	public Tutor()
+	public Tutor(String sNum, String n, String pass, ArrayList<String> c, int g, ArrayList<Lesson> l, ArrayList<Request> r)
 	{
-		
+		studentNumber=sNum;
+		name=n;
+		password=pass;
+		courses=c;
+		grade=g;
+		lessons=l;
+		requests=r;
+		occupied =false;
 	}
 
 	public String getStudentNumber()
 	{
-		
+		return studentNumber;
 	}
 	public String getName()
 	{
-		
+		return name;
 	}
 	public boolean verifyPassword(String pass)
 	{
-		
+		if(pass.equals(password))
+			return true;
+		return false;
 	}
 	public ArrayList<String> getCourses()
 	{
-		
+		return courses;
 	}
 	public int getGrade()
 	{
-		
+		return grade;
 	}
 	public Schedule getSchedule()
 	{
-		
+		return schedule;
 	}
 	public ArrayList<Lesson> getLessons()
 	{
-		
+		return lessons;
 	}
 	public ArrayList<Request> getRequests()
 	{
-		
+		return requests;
 	}
 	public boolean isOccupied()
 	{
-		
+		return occupied;
 	}
 
 	public void setStudentNumber(String sNum)
 	{
-		
+		studentNumber = sNum;
 	}
 	public void setName(String n)
 	{
-		
+		name=n;
 	}
 	public void setPassword(String pass)
 	{
-		
+		password=pass;
 	}
-	public void setCourses(ArrayList<String> courses)
+	public void setCourses(ArrayList<String> c)
 	{
-		
+		courses=c;
 	}
 	public void addCourses(String course)
 	{
-		
+		courses.add(course);
 	}
 	public void removeCourses(String course)
 	{
-		
+		courses.remove(course);
 	}
 	public void setGrade(int g)
 	{
-		
+		grade=g;
 	}
 	public void setSchedule(Schedule s)
 	{
-		
+		schedule =s;
 	}
 	public void addLesson(Lesson l)
 	{
-		
+		lessons.add(l);
 	}
 	public void removeLesson(Lesson l)
 	{
-		
+		lessons.remove(l);
 	}
 	public void addRequest(Request r)
 	{
-		
+		requests.add(r);
 	}
 	public void removeRequest(Request r)
 	{
-		
+		requests.remove(r);
 	}
 	public void setOccupied(boolean isOccupied)
 	{
-		
+		occupied=isOccupied;
 	}
 
 	public String toString()
 	{
-		
+		return "Name: " + name +"/n studentNumber: " +studentNumber + 
+				"/n Courses: " + courses.toString() + "/n Lessons: " + lessons.toString() +
+				"\n Requests: " + requests.toString() + "\n Occupied: " + occupied;
 	}
 }
